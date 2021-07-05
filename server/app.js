17abe06b8e0303fb2b51ae85a0d8f4a0544b7bb7
Lavisha Bhambri <bhambrilavisha@gmail.com>
2021-07-05 14:42:42 +0530

    Creating user schema

--- server/app.js
+++ server/app.js
@@ -1,2 +1,29 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const PORT = 5000;
+const {MONGOURI} = require('./keys');
+require('./models/user')
+
+app.get('/', (req, res)=>{
+  res.send("Hello world")
+})
+
+// Connect to DB
+mongoose.connect(MONGOURI,{
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+mongoose.connection.on('connected', ()=>{
+  console.log("Connected to mongoose");
+})
+
+mongoose.connection.on('error', (err)=>{
+  console.log("Error connecting to mongoose ", err);
+})
+
+
+app.listen(PORT,()=>{
+  console.log("server is running on port:", PORT);
+})
---
+++ server/keys.js
@@ -0,0 +1,3 @@
+module.exports={
+  MONGOURI:"mongodb+srv://lavisha:9AD6VbnrdM4Hz4wE@cluster0.abfdy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+}
